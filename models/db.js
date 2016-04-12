aa55ea2a126ee8a14792971f944e6fdb4b76ced2
Sonju Walker <sonjuwalker@gmail.com>
2016-04-12 14:31:01 -0500

    push up routes folder

---
+++ models/db.js
@@ -0,0 +1,18 @@
+  var mongoose = require('mongoose');
+
+// 1 - a connection string
+  var connectionString = 'mongodb://localhost/members';
+
+// 2 -  make connection to database
+  mongoose.connect(connectionString);
+
+// 3 - listen for events and log changes
+  mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + connectionString);
+  });
+  mongoose.connection.on('error', function(error) {
+    console.log('Mongoose connection error ' + error);
+  });
+  mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected from ' + connectionString);
+  });
